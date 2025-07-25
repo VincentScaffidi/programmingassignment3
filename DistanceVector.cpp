25e027e06205410c0edd01daac97b250ae00af6f
VincentScaffidi <a1833523@adelaide.edu.au>
2025-07-25 10:05:00 +0930

    Initial project setup and basic structure
    Starting with basic includes and structure
    Will implement Distance Vector routing algorithm
    Using C++ as I'm most familiar with it

--- DistanceVector.cpp
+++ DistanceVector.cpp
@@ -1,1 +1,27 @@
+#include <iostream>
+#include <string>
+#include <vector>
+#include <map>
 
+using namespace std;
+
+const int INF = 999999; // Large number to represent infinity
+
+class Router {
+public:
+    string name;
+    // TODO: Add distance table and routing logic
+    
+    Router(string n) : name(n) {}
+};
+
+class Network {
+public:
+    // TODO: Manage collection of routers and topology
+};
+
+int main() {
+    // TODO: Parse input and run algorithm
+    cout << "Distance Vector Implementation Started" << endl;
+    return 0;
+}
